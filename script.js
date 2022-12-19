83ced331e9ddbea5710aa67de63c897ec2124840
benjaminmq2013 <61093891+Benjaminmq2013@users.noreply.github.com>
2022-12-19 12:07:17 -0600

    Getting data from the user

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-// JS Code here...
+// Getting table containers
 
 const table_body = document.querySelector(".table-body")
-const add_button = document.getElementById("add-button")
+const form_submit = document.querySelector(".product-form")
 
-add_button.addEventListener( "click", (e) => buildElement(e) )
+// Getting Input fields
+
+const product_name = document.getElementById("product")
+const product_price = document.getElementById("price")
+const product_IVA = document.getElementById("iva")
+
+form_submit.addEventListener( "submit", (e) => buildElement(e) )
+
+const getRandom = () => {
+    return Math.floor(Math.abs(Math.random() * (1 - 999) + 1));
+}
 
 const buildElement = (e) => {
+
     e.preventDefault()
+
+    
+    // Generating Data
+
+    const random = getRandom()
+    const prod_name = product_name.value;
+    const prod_price = product_price.value;
+    const prod_IVA = product_IVA.value;
+
+    const ivaPercent = prod_price * Number(`0.${prod_IVA || 21}`) 
+    const total = Math.abs(ivaPercent - prod_price) ;   
+
+    const date = new Date().toLocaleDateString()
     
     const newElement = document.createElement("tr")
     
@@ -14,12 +38,12 @@ const buildElement = (e) => {
 
     let tableElement = `
     
-    <td>468</td>
-    <td>Monitor</td>
-    <td>$100.00</td>
-    <td>21%</td>
-    <td>$121.00</td>
-    <td>12/13/2022</td>
+    <td>${random}</td>
+    <td>${prod_name}</td>
+    <td>$${Number(prod_price).toFixed(2)}</td>
+    <td>${prod_IVA || 21}%</td>
+    <td>$${total.toFixed(2)}</td>
+    <td>${date}</td>
     <td>
         <button class="btn btn-delete">Delete</button>
     </td>`
